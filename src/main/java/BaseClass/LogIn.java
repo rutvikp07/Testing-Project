21d2d290e7ff1cbe547374a6bee0518a5dcd5330
Rutvik <rutvik2401@gmail.com>
2025-02-10 16:34:49 -0500

    After first class

--- src/main/java/BaseClass/LogIn.java
+++ src/main/java/BaseClass/LogIn.java
@@ -1,11 +1,15 @@
 
 package BaseClass;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LogIn {
 
 	public static void main(String[] args) {
 	
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://myflixerz.to/watch-movie/rockstar-5615.2522005");
 	}
 
 }
